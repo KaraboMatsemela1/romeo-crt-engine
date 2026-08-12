defc7d111ac5ebb995a874f0b3f8f53d48a7c798
KaraboMatsemela1 <matsemelakarabo@gmail.com>
2026-08-12 23:23:59 +0200

    storage: export local artifact adapter

--- src/romeo_crt_engine/storage/__init__.py
+++ src/romeo_crt_engine/storage/__init__.py
@@ -1,5 +1,6 @@
 """Provider-neutral storage contracts used by data and experiment layers."""
 
 from .contracts import ArtifactRef, ArtifactStore, DatasetRef
+from .local import LocalArtifactStore
 
-__all__ = ["ArtifactRef", "ArtifactStore", "DatasetRef"]
+__all__ = ["ArtifactRef", "ArtifactStore", "DatasetRef", "LocalArtifactStore"]
